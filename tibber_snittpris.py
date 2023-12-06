8c40d3b0c3be2063fbd6a6cd76e8739940f30326
Frode Ulriksen <frode.ulriksen@outlook.com>
2023-12-06 19:55:56 +0100

    new layout working

--- tibber_snittpris.py
+++ tibber_snittpris.py
@@ -1,24 +1,25 @@
 import datetime
-import asyncio
 from tibber_sql import SqlClass
 from writefile import TibberWriteFile
 from mqtt_class import MqttClass
 from mysecrets import SnittprisScrts
 from tibber_calculator import Calculation
 
-def main(start=14, end=9):
+def run(start=14, end=9):
     try:
         secrets = SnittprisScrts()
         sql_class = SqlClass(secrets.getip(), secrets.getusr(), secrets.getpwd(), secrets.getdb(), 'mysql_native_password', start)
         sql_class.get_data(start, end)
         processed_data = Calculation(sql_class.data, start)
-        asyncio.run(MqttClass("elec/python/mqtt/").sendtomqtt(processed_data.avg_std["now"], tolower=True))
+        MqttClass("elec/python/mqtt/").sendtomqtt(processed_data.avg_std["now"], tolower=True)
         sql_class.store_to_sql(sql_class.data, processed_data.avg_std)
     except KeyboardInterrupt as ki:
         TibberWriteFile().write_file('log.txt', f'[{datetime.datetime.now()}][INTERRUPT]: {ki}')
     except Exception as e:
         TibberWriteFile().write_file('log.txt', f'[{datetime.datetime.now()}][GENERIC]: {e}')
 
+def main():
+    run()
 
 if __name__ == "__main__":
     main()
